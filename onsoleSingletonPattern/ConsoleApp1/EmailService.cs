c8f55a6956c1c995b91bab3c08876d30381fdd70
Surjeet Singh <sb@primority.com>
2023-07-06 08:44:03 +0530

    added functionality

---
+++ onsoleSingletonPattern/ConsoleApp1/EmailService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	public class EmailService
+	{
+		private static EmailService _emailService;
+		private static readonly object _lock = new object();
+		private EmailService()
+		{
+
+		}
+		public static EmailService getInstance
+		{
+			get
+			{
+				lock(_lock)
+				{
+					if (_emailService == null)
+					{
+						_emailService = new EmailService();
+					}
+					return _emailService;
+				}
+			
+			}
+		}
+		private string getSMTPDetails()
+		{
+			return "smtp details";
+		}
+		public  void sendEmail()
+		{
+			Console.WriteLine(getSMTPDetails());
+			Console.WriteLine("Email sent");
+		}
+	}
+}
---
+++ onsoleSingletonPattern/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using OKXAPI_AA;
+
+AA_OKxClient obj = new AA_OKxClient();
+obj.SetApiCredentials("fd9aa94c-d7b9-43dc-9289-6095e17620b6", "B07E8D42EDEEB4F9589E0921959D93CC", "Developer1!");
+var test = obj.PlaceOrder("BTC-USDT", "cash", "buy", "limit", "0.01", "1000");
+Console.WriteLine(test);
+var singleObj = EmailService.getInstance;
+singleObj.sendEmail();
